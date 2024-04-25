4f8215b69f0927ba4a879dec324ad5e86f590f4d
nikolox1121 <fullyany1289@gmail.com>
2024-04-26 00:06:16 +0400

    commit

---
+++ day 25/classwork/1.py
@@ -0,0 +1,6 @@
+h = int(input("enter number :"))
+
+while h % 5 != 0:
+    h = input("enter number :")
+
+print("right")
---
+++ day 25/homework/p.py
@@ -0,0 +1,8 @@
+h = "Tbilisi2000"
+
+h2 = input("please enter your password :")
+
+while h != h2:
+    h2 = input("WRONG! enter the password again :")
+
+print("you have succesfully logged in to your account!")
