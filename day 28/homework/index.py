7593594e1cd058b0793d0239f7c1b299f013580a
nikolox1121 <fullyany1289@gmail.com>
2024-05-14 00:24:29 +0400

    commit

---
+++ day 28/homework/index.py
@@ -0,0 +1,3 @@
+students = ["gio" , "sandro" , "nikolozi" , "andria"]
+
+students = print("these are the best students in GOA 1." + students[0] + " then we have 2." + students[1] + " 3." + students[2] + " and 4." + students[3])
