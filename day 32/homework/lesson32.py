e8c811f586463a5fac99505257ebad843c7a2934
nikolox1121 <fullyany1289@gmail.com>
2024-06-12 01:17:28 +0400

    commit

---
+++ day 32/homework/lesson32.py
@@ -0,0 +1,6 @@
+new_goa = " "
+goa = "Goa Is The Best"
+for i in goa:
+    if i != " ":
+        new_goa += i
+print(new_goa)
---
+++ day 34/homework/lesson.py
@@ -0,0 +1,15 @@
+# 1)შექმნათ სია სადაც ჩაამატებთ თქვენი ყველა ჯგუფელის სახელს და გვარს, შემდეგ კი დაბეჭდავთ შუა 10 მოსწავლეს
+# 2)შექმენით სია რომელსაც გადასცემთ ანბანის ყველა ასოს, შემდეგ თქვენ ამ სიაში უნდა იპოვოთ g, o,  და a მოახდინოთ
+# ამ სამი ასოს კონკატინაცია ანუ შეერთება და დაბეჭდოთ goa
+
+goa_students = ["andria","giorgi","sandro","mate","lukhumi","toma","tazo","dato","luka","gabrieli","erekle","daviti","mariami","saba","giviko","deme","nika","gurami"]
+
+print(goa_students[4:14])
+
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R","S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+G = alphabet[6]
+O = alphabet[14]
+A = alphabet[0]
+
+print(G + O + A)
